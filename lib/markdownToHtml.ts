9b66c596347cead3558ec0968c42883c928ae9ee
AnupamaCodippily <avcodippily@gmail.com>
2021-04-05 22:09:05 +0530

    commit fast

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -2,7 +2,7 @@ import remark from 'remark'
 import html from 'remark-html'
 import prism from './remark-prism'
 
-export default async function markdownToHtml(markdown:any ) {
+export default async function markdownToHtml(markdown:string ) {
   // const withPrism = prism.highlight( markdown.toString(), prism.languages.javascript, 'javascript');
   const result = await remark().use(prism).use(html).process(markdown)
   return result.toString()
