b7b4d80ad1e5f014c07a86722210501885cd9113
AnupamaCodippily <avcodippily@gmail.com>
2021-04-05 13:48:18 +0530

    testing stuff

--- @types/remark-prism.d.ts
+++ @types/remark-prism.d.ts
@@ -1,4 +1,4 @@
-declare module "remark-prism" {
+declare module "../lib/remark-prism/remark-prism/src" {
     import { Plugin } from "unified";
     type RemarkPrism = Plugin;
     const remarkPrism: RemarkPrism;
