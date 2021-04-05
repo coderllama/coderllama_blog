34e506e615e1f410dcdfc79ea1da7b51285562e4
AnupamaCodippily <avcodippily@gmail.com>
2021-04-05 12:58:34 +0530

    testing stuff

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -5,5 +5,5 @@ import prism from 'prismjs'
 export default async function markdownToHtml(markdown:any ) {
   const withPrism = prism.highlight( markdown.toString(), prism.languages.javascript, 'javascript');
   const result = await remark().use(html).process(withPrism)
-  return result
+  return result.toString()
 }
