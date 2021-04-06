dd8dfbba5598c62b867aa28a374f0e613024ae88
AnupamaCodippily <avcodippily@gmail.com>
2021-04-06 23:29:54 +0530

    changing prism usage

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,23 +1,9 @@
 import remark from 'remark'
 import html from 'remark-html'
-import prism from './remark-prism'
+import prism from 'remark-prism'
 
 export default async function markdownToHtml(markdown:string ) {
-  // const withPrism = prism.highlight( markdown.toString(), prism.languages.javascript, 'javascript');
-  const result = await remark().use(prism, { plugins: [ 'command-line' ] }).use(html).process(markdown)
+  const result = await remark().use(prism).use(html).process(markdown)
   return result.toString()
 }
 
-//  export default async function markdownToHtml(markdown:any ) {
-//    const r = await require('unified')()
-//      .use(require('remark-parse'))
-//      .use(require('remark-stringify'))
-//      .use(require('remark-prism'), {
-//        /* options */
-//      })
-//      .use(require('remark-html'))
-//      .process(markdown);
-
-
-//   return r.toString()
-// }
