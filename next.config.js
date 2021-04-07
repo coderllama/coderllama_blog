d00eea432c47f4f07b7f1c33d617f9e73c3117df
AnupamaCodippily <avcodippily@gmail.com>
2021-04-07 20:12:50 +0530

    changed plugins

--- next.config.js
+++ next.config.js
@@ -4,23 +4,8 @@ module.exports = {
   future: {
     webpack5: true,
   },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Note: we provide webpack above so you should not `require` it
-    // Perform customizations to webpack config
-    // Important: return the modified config
-
-    // Example using webpack option
-    //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
-    config.node = {
-      fs: "empty",
-      net: "empty",
-      tls: "empty",
-    };
-    return config;
-  },
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
+  webpack: function (config, options) {
+    config.experiments = {};
     return config;
   },
 };
