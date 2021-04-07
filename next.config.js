caa7505488ead515ea6b01ce3c16f0b1e9f3383a
AnupamaCodippily <avcodippily@gmail.com>
2021-04-07 20:17:30 +0530

    changed plugins

--- next.config.js
+++ next.config.js
@@ -1,11 +1,28 @@
 const path =  require('path')
 
 module.exports = {
-  future: {
-    webpack5: true,
+  resolve : {
+      alias : {
+        prismjs : require.resolve('prismjs')
+      }    
   },
-  webpack: function (config, options) {
-    config.experiments = {};
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+
+    // Example using webpack option
+    //config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//))
+    config.node = {
+      fs: "empty",
+      net: "empty",
+      tls: "empty",
+    };
+    return config;
+  },
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
     return config;
   },
 };
